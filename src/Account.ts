b03c730ac782af00c986303ed562404015c746b9
Ilan <ilanolkies@outlook.com>
2021-10-02 18:51:22 -0300

    Implement proxy methods

--- src/Account.ts
+++ src/Account.ts
@@ -1,6 +1,7 @@
-import { Wallet } from 'ethers'
+import { Wallet, Signer, Bytes } from 'ethers'
+import { Provider, TransactionRequest } from "@ethersproject/abstract-provider"
 
-export class Account {
+export class Account extends Signer {
   wallet: Wallet
 
   get privateKey() {
@@ -8,10 +9,45 @@ export class Account {
   }
 
   get address() {
-    return this.wallet.address
+    return this.wallet.address.toLowerCase()
   }
 
   constructor({ privateKey }: { privateKey: string }) {
+    super()
     this.wallet = new Wallet(privateKey)
   }
+
+  getAddress(): Promise<string> {
+    return Promise.resolve(this.address)
+  }
+
+  // Returns the signed prefixed-message. This MUST treat:
+  // - Bytes as a binary message
+  // - string as a UTF8-message
+  // i.e. "0x1234" is a SIX (6) byte string, NOT 2 bytes of data
+  signMessage(message: Bytes | string): Promise<string> {
+    // this method is used only for direct signing messages
+    // ethers.js is not using this method in contracts or transactions
+    // we can proxy to the wallet and build the ux on top of it
+    return this.wallet.signMessage(message)
+  }
+
+  // Signs a transaxction and returns the fully serialized, signed transaction.
+  // The EXACT transaction MUST be signed, and NO additional properties to be added.
+  // - This MAY throw if signing transactions is not supports, but if
+  //   it does, sentTransaction MUST be overridden.
+  signTransaction(transaction: TransactionRequest): Promise<string> {
+    throw new Error()
+    // here we queue the transaction
+    // we should now wait for its semaphore
+    // ux happens here
+    // const signedTransaction = this.wallet.signTransaction(transaction)
+    // remove from queue
+    // release locks
+    // return signedTransaction
+  }
+
+  // Returns a new instance of the Signer, connected to provider.
+  // This MAY throw if changing providers is not supported.
+  connect(provider: Provider): Signer { throw new Error() }
 }
--- test/Account.test.ts
+++ test/Account.test.ts
@@ -1,14 +1,36 @@
 import { Account } from '../src/Account'
-import { private_key_testnet_0, private_key_testnet_1, address_testnet_0, address_testnet_1 } from './test-case'
+import { private_key_testnet_0, private_key_testnet_1, address_testnet_0, address_testnet_1, message, sig_testnet_0 } from './test-case'
 
 describe('Wallet', () => {
-  test('has an address', () => {
-    const account = new Account({ privateKey: private_key_testnet_0 })
-    expect(account.address.toLowerCase()).toEqual(address_testnet_0)
+  describe('info', () => {
+    test('has an address', () => {
+      const account = new Account({ privateKey: private_key_testnet_0 })
+      expect(account.address).toEqual(address_testnet_0)
+    })
+
+    test('creates the correct address', () => {
+      const account = new Account({ privateKey: private_key_testnet_1 })
+      expect(account.address).toEqual(address_testnet_1)
+    })
   })
 
-  test('creates the correct address', () => {
-    const account = new Account({ privateKey: private_key_testnet_1 })
-    expect(account.address.toLowerCase()).toEqual(address_testnet_1)
+  describe('proxy methods', function (this: {
+    account: Account
+  }) {
+    beforeAll(() => {
+      this.account = new Account({ privateKey: private_key_testnet_0 })
+    })
+
+    test('returns account', async () => {
+      expect(await this.account.getAddress()).toEqual(address_testnet_0)
+    })
+
+    test('signs messages', async () => {
+      expect(await this.account.signMessage(message)).toEqual(sig_testnet_0)
+    })
+
+    test('cannot change provider', () => {
+      expect(() => this.account.connect({} as any)).toThrow()
+    })
   })
 })
