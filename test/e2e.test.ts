b3f7a754dab103475e0826fabdfea52d04a6f3c3
Ilan <ilanolkies@outlook.com>
2021-10-18 01:57:47 -0300

    Add comments and delete old code

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -2,25 +2,33 @@ import { deploySmartWalletFactory, sendAndWait, fundAccount, testJsonRpcProvider
 import { KeyManagementSystem } from '../src/KeyManagementSystem'
 import { SmartWalletFactory } from '../src/SmartWalletFactory'
 import { RIFWallet } from '../src/RIFWallet'
+import { BigNumber } from '@ethersproject/bignumber'
 
 describe('e2e', () => {
   test('local account', async () => {
+    // deployment of smart contracts
     const smartWalletFactoryContract = await deploySmartWalletFactory()
 
+    // account creation
     const kms = KeyManagementSystem.create()
     const walletRequest = kms.nextWallet(31)
 
+    // user confirmation
     walletRequest.save()
 
+    // funding account
     const wallet = walletRequest.wallet.connect(testJsonRpcProvider)
     await sendAndWait(fundAccount(wallet.address))
 
+    // smart wallet contract creation
     const smartWalletFactory = await SmartWalletFactory.create(wallet, smartWalletFactoryContract.address)
     await sendAndWait(smartWalletFactory.deploy())
 
+    // rif wallet sdk
     const smartWalletAddress = await smartWalletFactory.getSmartWalletAddress()
     const rifWallet = RIFWallet.create(wallet, smartWalletAddress)
 
+    // send transaction
     const txRequest = {
       to: '0x0000000000111111111122222222223333333333',
       data: '0xabcd'
@@ -28,11 +36,25 @@ describe('e2e', () => {
 
     const txPromise = rifWallet.sendTransaction(txRequest)
 
-    rifWallet.nextRequest().confirm()
+    // transaction is queued, waiting for confirmation
+    const nextTx = rifWallet.nextRequest()
 
+    expect(nextTx.type).toEqual('sendTransaction')
+
+    // user changes transaction params
+    const gasPrice = BigNumber.from('100000')
+    nextTx.payload.transactionRequest.gasPrice = gasPrice
+
+    // user confirms
+    nextTx.confirm()
+
+    // transaction is sent
     const tx = await txPromise
+
+    // transaction is confirmed (mined)
     const receipt = await tx.wait()
 
+    expect(tx.gasPrice).toEqual(gasPrice)
     expect(receipt.status).toBe(1)
   })
 })
