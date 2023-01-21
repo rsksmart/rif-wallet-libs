e6c8eb7b4b8816c3bd03386dd4e53636421a416d
rodrigoncalves <rodrigosg2000@gmail.com>
2023-01-21 18:06:03 -0300

    fix lint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false,
+  bracketSpacing: true,
+};
