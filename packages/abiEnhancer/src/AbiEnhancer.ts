37d5aafa952f20aed5f19ca8df338de8b93f6c21
Francis Rodriguez <francisngg@gmail.com>
2023-01-26 11:54:53 -0400

    Linted AbiEnhancer.ts

--- packages/abiEnhancer/src/AbiEnhancer.ts
+++ packages/abiEnhancer/src/AbiEnhancer.ts
@@ -1,8 +1,6 @@
 import { TransactionRequest } from '@ethersproject/abstract-provider'
 import { Signer } from '@ethersproject/abstract-signer'
-import { ERC20EnhanceStrategy } from './strategies/ERC20EnhanceStrategy'
-import { OtherEnhanceStrategy } from './strategies/OtherEnhanceStrategy'
-import { RBTCEnhanceStrategy } from './strategies/RBTCEnhanceStrategy'
+import { ERC20EnhanceStrategy, OtherEnhanceStrategy, RBTCEnhanceStrategy } from './strategies'
 
 export interface FunctionParameter {
   name: string
@@ -37,17 +35,17 @@ export interface IAbiEnhancer {
 export class AbiEnhancer implements IAbiEnhancer {
   public strategies: EnhanceStrategy[]
 
-  constructor() {
+  constructor () {
     this.strategies = [
       new ERC20EnhanceStrategy(),
       new OtherEnhanceStrategy(),
-      new RBTCEnhanceStrategy(),
+      new RBTCEnhanceStrategy()
     ]
   }
 
-  public async enhance(
+  public async enhance (
     signer: Signer,
-    transactionRequest: TransactionRequest,
+    transactionRequest: TransactionRequest
   ): Promise<EnhancedResult | null> {
     for (const strategy of this.strategies) {
       const result = await strategy.parse(signer, transactionRequest)
