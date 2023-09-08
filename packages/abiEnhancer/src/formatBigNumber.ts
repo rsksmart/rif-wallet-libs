6ba2441e75505d728307f25b36d3c6f3ad3525ac
Rodrigo Gonçalves <rodrigo.santana@iovlabs.org>
2023-09-07 23:49:08 -0300

    add optional precision param

--- packages/abiEnhancer/src/formatBigNumber.ts
+++ packages/abiEnhancer/src/formatBigNumber.ts
@@ -1,22 +1,26 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
-export const formatBigNumber = (amount: BigNumber, decimals: number) => {
+export const formatBigNumber = (
+  amount: BigNumber,
+  decimals: number,
+  precision = 8,
+) => {
   if (amount.isZero()) {
     return '0'
   }
 
   const divisor = BigNumber.from(10).pow(BigNumber.from(decimals))
-
   const quotient = amount.div(divisor)
   const rest = amount.mod(divisor)
   const decimalsPart = rest
     .toString()
     .padStart(decimals, '0')
-    .slice(0, 8)
+    .slice(0, precision)
+    // https://stackoverflow.com/questions/5774246/remove-trailing-characters-from-string-in-javascript
     .replace(/0+$/, '')
 
-  // https://stackoverflow.com/questions/5774246/remove-trailing-characters-from-string-in-javascript
   return (
-    quotient.toString() + (rest.isZero() || !decimalsPart ? '' : '.' + decimalsPart)
+    quotient.toString() +
+    (rest.isZero() || !decimalsPart ? '' : '.' + decimalsPart)
   )
 }
