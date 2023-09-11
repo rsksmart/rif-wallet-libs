01a150763152350606bd719ec0d1227fbbac6801
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2023-09-11 12:31:09 -0300

    Fix format big number for small amounts (#86)
    * fix format big number for small amounts
    
    * add optional precision param
    
    * add some tests
    
    * Bump version and add test for zero.
    
    * lint
    
    ---------
    
    Co-authored-by: Jesse Clark <hello@developerjesse.com>

--- packages/abiEnhancer/src/formatBigNumber.ts
+++ packages/abiEnhancer/src/formatBigNumber.ts
@@ -1,21 +1,26 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
-export const formatBigNumber = (amount: BigNumber, decimals: number) => {
+export const formatBigNumber = (
+  amount: BigNumber,
+  decimals: number,
+  precision = 8,
+) => {
   if (amount.isZero()) {
     return '0'
   }
 
   const divisor = BigNumber.from(10).pow(BigNumber.from(decimals))
-
   const quotient = amount.div(divisor)
   const rest = amount.mod(divisor)
+  const decimalsPart = rest
+    .toString()
+    .padStart(decimals, '0')
+    .slice(0, precision)
+    // https://stackoverflow.com/questions/5774246/remove-trailing-characters-from-string-in-javascript
+    .replace(/0+$/, '')
 
-  // https://stackoverflow.com/questions/5774246/remove-trailing-characters-from-string-in-javascript
   return (
     quotient.toString() +
-    (rest.isZero()
-      ? ''
-      : '.' +
-        rest.toString().padStart(decimals, '0').slice(0, 8).replace(/0+$/, ''))
+    (rest.isZero() || !decimalsPart ? '' : '.' + decimalsPart)
   )
 }
