dcb75c265c95e12fbb62a66a448267bd63da19ef
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2023-11-28 13:49:32 -0300

    bring getAllTokens back

--- packages/token/src/tokenMetadata.ts
+++ packages/token/src/tokenMetadata.ts
@@ -1,9 +1,11 @@
 import { Signer } from '@ethersproject/abstract-signer'
 import contractMapTestNet from '@rsksmart/rsk-testnet-contract-metadata'
+import { IToken } from './BaseToken'
 import { ERC20Token } from './ERC20Token'
 import { RBTCToken } from './RBTCToken'
-import { IToken } from './types/IToken'
+import { ITokenWithBalance } from './types/ITokenWithBalance'
 
+const contractMapMainnet = require('@rsksmart/rsk-contract-metadata')
 const rbtcMainnet = require('./assets/RBTC-mainnet.svg')
 const rbtcTestnet = require('./assets/RBTC-testnet.svg')
 
@@ -19,9 +21,38 @@ export interface ITokenMetadata {
 
 export const MAINNET_CHAINID = 30
 
+export const tokensMetadataMainnet = contractMapMainnet as ITokenMetadata
 export const tokensMetadataTestnet = contractMapTestNet as ITokenMetadata
 
-export const convertToERC20Token = (token: IToken, signer: Signer) => {
+export const getAllTokens = async (signer: Signer): Promise<IToken[]> => {
+  const chainId = await signer.getChainId()
+
+  const metadataTokens =
+    chainId === MAINNET_CHAINID ? tokensMetadataMainnet : tokensMetadataTestnet
+
+  const metadataKeys = Object.keys(metadataTokens)
+
+  const tokens: IToken[] = []
+
+  const rbtc = makeRBTCToken(signer, chainId)
+
+  tokens.push(rbtc)
+
+  for (const address of metadataKeys) {
+    const addressWithoutChecksum = address.toLowerCase()
+    const symbol = metadataTokens[address].symbol
+    const token = new ERC20Token(addressWithoutChecksum, signer, symbol, '')
+
+    tokens.push(token)
+  }
+
+  return tokens
+}
+
+export const convertToERC20Token = (
+  token: ITokenWithBalance,
+  signer: Signer,
+) => {
   const addressWithoutChecksum = token.contractAddress.toLowerCase()
   return new ERC20Token(addressWithoutChecksum, signer, token.symbol, '')
 }
