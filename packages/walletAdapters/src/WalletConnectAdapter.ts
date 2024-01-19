e6795725289bb359d255623a88fbab9ca0506b0b
Francis Rodriguez <francisngg@gmail.com>
2024-01-19 14:57:22 -0400

    US-2105 Will now allow the user to pass resolvers to the WalletConnectAdapter.ts.

--- packages/walletAdapters/src/WalletConnectAdapter.ts
+++ packages/walletAdapters/src/WalletConnectAdapter.ts
@@ -1,14 +1,18 @@
 import { Signer } from '@ethersproject/abstract-signer'
 import { RIFWallet } from '@rsksmart/rif-wallet-core'
 import { PersonalSignResolver, SendTransactionResolver, SignTypedDataResolver } from './resolvers'
-import { RPCAdapter } from './RPCAdapter'
+import { IResolver, RPCAdapter } from './RPCAdapter'
 
 export class WalletConnectAdapter extends RPCAdapter {
-  constructor(signer: Signer) {
-    super([
-      new SendTransactionResolver(signer),
-      new PersonalSignResolver(signer),
-      new SignTypedDataResolver(signer as unknown as RIFWallet),
-    ])
+  constructor(signer: Signer, resolvers: IResolver[] = []) {
+    let resolversToUse = resolvers
+    if (resolvers.length === 0) {
+      resolversToUse = [
+        new SendTransactionResolver(signer),
+        new PersonalSignResolver(signer),
+        new SignTypedDataResolver(signer as RIFWallet),
+      ]
+    }
+    super(resolversToUse)
   }
 }
